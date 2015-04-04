66977b74b1c330fdc8c7677d5c7f8cb64ea55126
rodrigolopez <rodrigo@wime.com.mx>
2015-04-04 11:40:52 +0200

    Se aplica el refactor "extract method" para el metodo start en el StartController.

--- src/main/java/controllers/StartController.java
+++ src/main/java/controllers/StartController.java
@@ -4,27 +4,34 @@ import models.Card;
 import models.Deck;
 
 public class StartController extends KlondikeController {
-	
+
 	public void start() {
-		//Create Tableau
-		for(int i=0;i<this.NUM_TABLEAU;i++){
+		this.createTableau();
+		this.createFoundations();
+		this.createDraw();
+	}
+
+	private void createTableau() {
+		for (int i = 0; i < this.NUM_TABLEAU; i++) {
 			Deck tableau = new Deck();
-			for(int j=0;j<=i;j++){
-				if(j==i)
+			for (int j = 0; j <= i; j++) {
+				if (j == i)
 					tableau.add(new Card(true));
 				else
 					tableau.add(new Card(false));
 			}
 			this.tableaus.add(tableau);
 		}
-		
-		//Create Foundations
-		for(int i=0;i<this.NUM_FOUNDATIONS;i++){
+	}
+
+	private void createFoundations() {
+		for (int i = 0; i < this.NUM_FOUNDATIONS; i++) {
 			foundations.add(new Deck());
 		}
-						
-		//Create draw
-		for(int i=0;i<this.NUM_DRAW;i++){
+	}
+
+	private void createDraw() {
+		for (int i = 0; i < this.NUM_DRAW; i++) {
 			draw.add(new Card(false));
 		}
 	}
